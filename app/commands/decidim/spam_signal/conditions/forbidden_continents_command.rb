fccfaf8ae6841c0e0679e79f3af6c3371cdca9df
Renato <renato@octree.ch>
2025-05-20 04:31:35 -0300

    feat: command for forbidden continents

--- app/commands/decidim/spam_signal/conditions/forbidden_continents_command.rb
+++ app/commands/decidim/spam_signal/conditions/forbidden_continents_command.rb
@@ -5,18 +5,20 @@ module Decidim
     module Conditions
       class ForbiddenContinentsCommand < ConditionHandler
         def call
-          return broadcast(:invalid) if forbidden_continent?(continent)
+          return broadcast(:invalid) if forbidden_continent?
 
           broadcast(:valid)
         end
 
         private
 
-        def forbidden_continent?(continent)
-          true
+        def forbidden_continent?
+          forbidden_continents_list.include?(Current.continent)
         end
 
-        
+        def forbidden_continents_list
+          @config["forbidden_continents_list"]
+        end
       end
     end
   end
--- lib/decidim/overrides/application_controller_overrides.rb
+++ lib/decidim/overrides/application_controller_overrides.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
-# This module overrides the `update` method in the AccountController.
-# By using `prepend` the module's `update` method takes precedence
-# over the original method in the controller. 
-# It handles the outcome of the update operation through callbacks.
-# Additional callback added to the original:
-# - On spam detection:
-#   - Displays an alert message with the relevant error details.
-#   - Redirects the user back to their account page.
+# This module 
 module Decidim
   module ApplicationControllerOverrides
     extend ActiveSupport::Concern
