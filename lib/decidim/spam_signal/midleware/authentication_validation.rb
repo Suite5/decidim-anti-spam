37c76430adbdbc0f9884ed6b587cdcf3c3cefd1c
Hadrien Froger <hadrien@octree.ch>
2025-06-04 17:06:47 +0100

    fix: on lock, redirect to terms and conditions page and unsign the user

--- lib/decidim/spam_signal/midleware/authentication_validation.rb
+++ lib/decidim/spam_signal/midleware/authentication_validation.rb
@@ -22,8 +22,27 @@ module Decidim
           # Fire authentication with a dummy active model, to keep the same logic
           # as other flows
           ::Decidim::SpamSignal::Flows::AuthenticationFlow::DummyUser.new(current_organization, current_user).validate
-          @app.call(env)
+          response = @app.call(env)
+          # If a lock has been performed, redirect to the terms of service page and display a flash message
+          # to the user.
+          if Decidim::SpamSignal.spam_actions_performed.include?(:lock)
+            env["warden"]&.user("user")
+            location = root_path(env, current_organization.host)
+            [301, { "Location" => location, "Content-Type" => "text/html", "Content-Length" => "0" }, []]  
+          else 
+            response
+          end
         end
+
+
+        private
+
+        def root_path(env, host)
+          request = Rack::Request.new(env)
+          url = URI("/pages/terms-of-service")
+          url.host = host
+          url.to_s
+        end  
       end
     end
   end
