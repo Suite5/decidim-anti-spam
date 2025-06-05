98f4c255f1aaea3b1904589346c60924ad393ba0
Hadrien Froger <hadrien@octree.ch>
2025-06-05 11:01:49 +0100

    fix: fix lock action regression

--- lib/decidim/spam_signal/midleware/authentication_validation.rb
+++ lib/decidim/spam_signal/midleware/authentication_validation.rb
@@ -22,7 +22,23 @@ module Decidim
           # Fire authentication with a dummy active model, to keep the same logic
           # as other flows
           ::Decidim::SpamSignal::Flows::AuthenticationFlow::DummyUser.new(current_organization, current_user).validate
-          @app.call(env)
+          result = @app.call(env)
+          if ::Decidim::SpamSignal.spam_actions_performed.include?(:hide_authentication)
+            location = terms_and_conditions(env, current_organization.host)
+            return [301, { "Location" => location, "Content-Type" => "text/html", "Content-Length" => "0" }, []]
+          else
+            result
+          end
+        end
+
+
+        private
+
+        def terms_and_conditions(env, host)
+          request = Rack::Request.new(env)
+          url = URI("/pages/terms-of-service")
+          url.host = host
+          url.to_s  
         end
       end
     end
